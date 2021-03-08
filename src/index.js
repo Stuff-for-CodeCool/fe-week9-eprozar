1af0c5edef5f011b11f57894b47dd1cb165843af
alexsincai <alex.sincai@yahoo.co.uk>
2021-03-08 15:57:20 +0200

    Deploy

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import "./index.css";
 
 import App from "./App.jsx";
 
-const base = window.location.href.slice(
-    0,
-    window.location.href.lastIndexOf("/")
-);
-document.head.querySelector("base").href = base;
+
 
 render(<App />, document.getElementById("root"));
